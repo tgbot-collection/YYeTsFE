7952e7e2e33e8320e9c6a0e6a07a9428017192f0
zuiyu <wyx1818@vip.qq.com>
2021-06-15 16:45:14 +0800

    perf: 为 sentry 生成 sourcemap

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # YYeTs
 
 YYeTsBot 前端
+
+React App
+
+自适应 PC、移动端，亮色、暗色模式
+
+## 部署
+
+1. 安装依赖
+
+    ```shell
+    yarn
+    ```
+
+1. 复制 `.env.example` 为 `.env`，修改其中的变量
+
+    ```dotenv
+    # 接口域名
+    REACT_APP_DOMAIN=xxx
+    # google analytic 追踪 ID
+    REACT_APP_GA=xxx
+    # Sentry DSN
+    REACT_APP_SENTRY_DSN=xxx
+    # 是否生成 sourcemap
+    GENERATE_SOURCEMAP=true
+    ```
+
+1. build 项目
+
+    ```shell
+    yarn build
+    ```
--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,16 @@ import { Integrations } from "@sentry/tracing";
 
 import App from "./app/App";
 
-Sentry.init({
-  dsn: process.env.REACT_APP_SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
+if (process.env.NODE_ENV === "development") {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
