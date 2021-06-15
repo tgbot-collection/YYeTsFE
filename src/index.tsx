9666edb0446bd7cf0465cf71f1d14a66b0677ebd
zuiyu <wyx1818@vip.qq.com>
2021-06-15 14:56:23 +0800

    feat: 引入 sentry 监控

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import App from "./app/App";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
