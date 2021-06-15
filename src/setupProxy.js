4591ed8a0d6edff280a88bcd9d49e69ba1b04286
zuiyu <wyx1818@vip.qq.com>
2021-06-15 19:45:16 +0800

    chore: 移除无用代码

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,4 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const AES = require("crypto-js/aes");
-
-function genNe1(id) {
-  return AES.encrypt(`/api/resource?id=${id}`, `${id}`).toString();
-}
 
 module.exports = function setProxy(app) {
   app.use(
@@ -11,13 +6,6 @@ module.exports = function setProxy(app) {
     createProxyMiddleware({
       target: process.env.REACT_APP_DOMAIN,
       changeOrigin: true,
-      onProxyReq(proxyReq, req) {
-        const { id } = req.query;
-
-        if (id) {
-          proxyReq.setHeader("ne1", genNe1(id));
-        }
-      },
     })
   );
 };
