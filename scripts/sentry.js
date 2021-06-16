760383b8fc3b23c2e8113f46d18619bfd73149b7
zuiyu <wyx1818@vip.qq.com>
2021-06-16 10:42:46 +0800

    fix: 修正 sentry release 错误

--- scripts/sentry.js
+++ scripts/sentry.js
@@ -5,7 +5,7 @@ const fse = require("fs-extra");
 
 async function createReleaseAndUpload() {
   const date = new Date();
-  const release = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} `;
+  const release = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
   const cli = new SentryCli();
 
