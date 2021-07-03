1814ffe70adc04a675f6bf5c2dd2e56675744df7
zuiyu <wyx1818@vip.qq.com>
2021-07-03 09:27:12 +0800

    feat: 新增批量复制电驴

--- src/app/pages/Resource/DataTable.tsx
+++ src/app/pages/Resource/DataTable.tsx
@@ -14,8 +14,8 @@ import {
   Tooltip,
   Typography,
   Button,
+  SvgIcon,
 } from "@material-ui/core";
-import CloudDownloadIcon from "@material-ui/icons/CloudDownload";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FileCopy as FileCopyIcon, Cloud as CloudIcon } from "@material-ui/icons";
 import { useSnackbar, ProviderContext } from "notistack";
@@ -52,7 +52,10 @@ interface EnhancedTableToolbarProps {
   numSelected: number;
   rowCount: number;
   onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  multipleAddress: string;
+  multipleAddress: {
+    ed2k: string;
+    magnet: string;
+  };
   enqueueSnackbar: ProviderContext["enqueueSnackbar"];
 }
 
@@ -82,20 +85,46 @@ const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
         </Typography>
       )}
       {numSelected > 0 && (
-        <CopyToClipboard
-          text={multipleAddress}
-          onCopy={() => {
-            enqueueSnackbar("批量地址已复制", {
-              variant: "success",
-            });
-          }}
-        >
-          <Tooltip title="复制磁链">
-            <IconButton aria-label="delete">
-              <CloudDownloadIcon />
-            </IconButton>
-          </Tooltip>
-        </CopyToClipboard>
+        <>
+          <CopyToClipboard
+            text={multipleAddress.ed2k}
+            onCopy={() => {
+              enqueueSnackbar("批量地址已复制", {
+                variant: "success",
+              });
+            }}
+          >
+            <Tooltip title="复制电驴">
+              <IconButton aria-label="delete">
+                <SvgIcon viewBox="0 0 1024 1024">
+                  <path
+                    d="M797.547 142.933s-54.72-90.56-99.733-93.013c0 0 8.96 78.613 33.813 127.68 0 0-108.48 120.747-141.44 143.68-32.96 22.933-200.64 1.92-218.56-4.907-25.6-9.813-257.067-52.8-273.707 138.56-0.107 0.96-0.107 2.027-0.213 2.987-23.253 161.28-54.613 185.813-64.32 200.533 0 0 28.693 40.533 71.467-147.307 0 0 19.307 101.227 24.96 146.56 1.6 13.12-24.107 41.707-24.107 41.707s-13.653 24.533-3.2 51.52c0 0 13.653 87.467 6.4 149.653l-4.8 27.84h50.667l-6.4-38.507-2.453-170.987s73.92-34.347 98.027-127.68c-0.107 0.213-0.533 67.627 9.707 114.88 3.093-5.12-8.107 19.413 12.8 34.987l6.4 180.373 48.213 0.853-2.453-36.053s-24.107-133.653-21.653-166.4c0 0 61.013-58.987 65.92-108.053 0 0 141.44 27.84 183.253 18.027 0 0 20.053 126.933 35.307 165.333 0 0 21.653 48.32 16.853 103.147l-5.653 4.907v29.44h48.213v-29.227l-5.653-10.667-2.453-247.253 59.52 228.373-5.653 30.293v18.88h58.667l-4.053-22.933-11.2-42.56s-35.307-189.12-27.307-257.067c8-67.947 105.28-252.16 105.28-252.16s86.4 63.68 137.067 46.507c0 0 42.133-19.627 15.68-65.813l-43.2-84.053s-18.773-47.253-65.387-64.427c0 0 18.453-113.6 8.853-138.987 0 0-57.067 44.907-63.467 101.333z"
+                    p-id="3943"
+                  />
+                </SvgIcon>
+              </IconButton>
+            </Tooltip>
+          </CopyToClipboard>
+          <CopyToClipboard
+            text={multipleAddress.magnet}
+            onCopy={() => {
+              enqueueSnackbar("批量地址已复制", {
+                variant: "success",
+              });
+            }}
+          >
+            <Tooltip title="复制磁链">
+              <IconButton aria-label="delete" size="small">
+                <SvgIcon viewBox="0 0 1024 1024">
+                  <path
+                    d="M238.933 284.459v273.066a273.067 273.067 0 0 0 545.963 10.24l0.17-10.24V284.46h182.06v273.066c0 251.307-203.777 455.083-455.126 455.083S56.875 808.875 56.875 557.525V284.46h182.058zM967.125 11.392v182.016H785.024l0.043-182.016h182.058z m-910.25 0h182.058v182.016H56.832V11.392z"
+                    p-id="2842"
+                  />
+                </SvgIcon>
+              </IconButton>
+            </Tooltip>
+          </CopyToClipboard>
+        </>
       )}
     </Toolbar>
   );
@@ -142,11 +171,15 @@ const HrefButton = (props: ButtonProps) => {
     <CopyToClipboard
       text={downItem.passwd || "无密码"}
       onCopy={() => {
-        enqueueSnackbar("网盘密码已复制", {
+        enqueueSnackbar(`已复制网盘密码： ${downItem.passwd || "无密码"}`, {
           variant: "success",
         });
         gtag("event", "download", { resource_id: resourceId, type: downItem.way_cn });
         postMetrics("download").catch();
+        setTimeout(() => {
+          // eslint-disable-next-line no-restricted-globals
+          location.href = downItem.address;
+        }, 1000);
       }}
     >
       <Button
@@ -154,6 +187,7 @@ const HrefButton = (props: ButtonProps) => {
         size="small"
         startIcon={<CloudIcon />}
         href={downItem.address}
+        onClick={(e) => e.preventDefault()}
         title={`网盘密码：${downItem.passwd || "无密码"}`}
         color="primary"
         disableRipple
@@ -233,9 +267,9 @@ export function DataTableComponent(props: DataTablePropTypes) {
     setSelected([]);
   };
 
-  const handleClickMultipleDown = () =>
+  const handleClickMultipleDown = (way: "1" | "2") =>
     selected.reduce((pre, current) => {
-      const add = tableData[current].files?.find((address) => address?.way === "2");
+      const add = tableData[current]?.files?.find((address) => address?.way === way);
       return pre + (add ? `${add.address}\n` : "");
     }, "");
 
@@ -257,7 +291,7 @@ export function DataTableComponent(props: DataTablePropTypes) {
           numSelected={selected.length}
           rowCount={tableData.length}
           onSelectAllClick={handleSelectAllClick}
-          multipleAddress={handleClickMultipleDown()}
+          multipleAddress={{ magnet: handleClickMultipleDown("2"), ed2k: handleClickMultipleDown("1") }}
           enqueueSnackbar={enqueueSnackbar}
         />
         <TableContainer>
