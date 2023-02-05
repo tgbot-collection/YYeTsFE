28689798464c16fb21f87454d4c9bd624bf690fe
Benny <benny.think@gmail.com>
2023-02-05 20:33:38 +0100

    add svg for UA

---
+++ src/component/CommentCard/tinyIcon.ts
@@ -0,0 +1,34 @@
+import chrome from "super-tiny-icons/images/svg/chrome.svg";
+import firefox from "super-tiny-icons/images/svg/firefox.svg";
+import safari from "super-tiny-icons/images/svg/safari.svg";
+import edge from "super-tiny-icons/images/svg/edge.svg";
+import opera from "super-tiny-icons/images/svg/opera.svg";
+import chromium from "super-tiny-icons/images/svg/chromium.svg";
+
+import android from "super-tiny-icons/images/svg/android.svg";
+import apple from "super-tiny-icons/images/svg/apple.svg";
+import ubuntu from "super-tiny-icons/images/svg/ubuntu.svg";
+import debian from "super-tiny-icons/images/svg/debian.svg";
+import linux from "super-tiny-icons/images/svg/linux.svg";
+import macos from "super-tiny-icons/images/svg/macos.svg";
+import windows from "super-tiny-icons/images/svg/windows.svg";
+import ie from "../../svg/ie.svg";
+import unknown from "../../svg/unknown.svg";
+
+export {
+  chrome,
+  firefox,
+  safari,
+  edge,
+  opera,
+  chromium,
+  android,
+  apple,
+  ubuntu,
+  debian,
+  linux,
+  macos,
+  windows,
+  ie,
+  unknown,
+};
