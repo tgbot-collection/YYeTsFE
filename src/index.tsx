d4117e9f4230a0d53768299a0ff1ad90ec8b9a56
Benny <benny.think@gmail.com>
2023-04-25 21:48:14 +0200

    use self-hosted sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import * as Sentry from "@sentry/browser";
 
 import App from "./app/App";
 import { SplashScreenProvider } from "./layout";
@@ -9,13 +8,6 @@ import { SplashScreenProvider } from "./layout";
 if (process.env.NODE_ENV !== "development" && process.env.REACT_APP_SENTRY_RELEASE) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
-    integrations: [new Integrations.BrowserTracing()],
-    release: process.env.REACT_APP_SENTRY_RELEASE,
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 0.2,
   });
 }
 
