5807eba2b857f2b9ae87299b4578da34824fca01
Benny <benny.think@gmail.com>
2024-12-06 22:15:19 +0100

    支持一下

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import GitHubButton from "react-github-btn";
-import { Button, Container, Link, Typography } from "@material-ui/core";
+import { Button, Container, Link, Typography, Popover, Box, Grid, Chip } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 import { Adsense } from "@ctrl/react-adsense";
 
@@ -11,6 +11,8 @@ import { LastComment } from "./LastComment";
 import { Announce } from "./Announce";
 
 export function HomePage() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
   setTitle("首页");
 
   const classes = useStyles();
@@ -19,6 +21,16 @@ export function HomePage() {
     postMetrics("home").catch(noop);
   }, []);
 
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
   return (
     <>
       <Container maxWidth="md" className={classes.content}>
@@ -30,7 +42,7 @@ export function HomePage() {
           <Typography variant="h5" component="p" color="textSecondary" gutterBottom>
             本站数据库
             <Link component={RouterLink} to="/database" color="secondary">
-              可提供下载
+              可下载
             </Link>
             ， 如有疑问可以查看
             <Link component={RouterLink} to="/help">
@@ -56,44 +68,69 @@ export function HomePage() {
         </div>
 
         <div className={classes.sponsor}>
-          <Typography style={{ marginRight: "16px" }}>觉得不错，支持一下 </Typography>
+          <Typography style={{ marginRight: "16px" }} onMouseEnter={handleClick} onClick={handleClick}>
+            觉得不错，
+            <Link>支持一下</Link>
+          </Typography>
 
-          <Link href="https://afdian.net/@BennyThink" style={{ transform: "translate3d(0, 3px,0)" }}>
-            <img
-              src={toAbsoluteUrl("/sponsor/afdian.png")}
-              style={{
-                transform: "scale(0.9)",
-                borderRadius: "50%",
-              }}
-              alt="afdian"
-              className="img"
-            />
-          </Link>
-          <Link href="https://www.buymeacoffee.com/bennythink">
-            <img
-              src={toAbsoluteUrl("/sponsor/coffee.jpg")}
-              style={{
-                marginLeft: "24px",
-                transform: "scale(0.9)",
-                borderRadius: "50%",
-              }}
-              alt="coffee"
-              className="img"
-            />
-          </Link>
-
-          <Link href="https://buy.stripe.com/dR67vU4p13Ox73a6oq">
-            <img
-              src={toAbsoluteUrl("/sponsor/stripe.png")}
-              style={{
-                marginLeft: "24px",
-                transform: "scale(0.9)",
-                borderRadius: "50%",
-              }}
-              alt="stripe"
-              className="img"
-            />
-          </Link>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            PaperProps={{ style: { width: "75%", padding: "2rem" } }}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <Grid container spacing={5}>
+              <Grid item xs={4}>
+                <Typography variant="h4" gutterBottom>
+                  Stripe（支付宝）
+                </Typography>
+                <img
+                  src="https://raw.githubusercontent.com/tgbot-collection/YYeTsBot/master/assets/CNY.png"
+                  style={{
+                    width: "10rem",
+                  }}
+                  alt="Stripe"
+                />
+                <Typography gutterBottom>
+                  <Link href="https://buy.stripe.com/dR67vU4p13Ox73a6oq">赞赏链接</Link>
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="h4" gutterBottom>
+                  加密货币
+                </Typography>
+                <Box display="flex">
+                  <Chip label="USDT-TRC20" variant="outlined" color="secondary" size="small" />
+                  <Typography gutterBottom>TL8kqCm9SwrV44qLaKvWbwrTtDN3sx5dVP</Typography>
+                </Box>
+                <Box display="flex">
+                  <Chip label="Toncoin" variant="outlined" color="secondary" size="small" />
+                  <Typography gutterBottom> UQBkXRAUVoEF2AA7QejHpsr3JmBWhsIfQTLURxJ3txc_rVFI</Typography>
+                </Box>
+                <Box display="flex">
+                  <Chip label="TRX" variant="outlined" color="secondary" size="small" />
+                  <Typography gutterBottom> TF9peZjC2FYjU4xNMPg3uP4caYLJxtXeJS</Typography>
+                </Box>
+                <Box display="flex">
+                  <Chip label="XLM" variant="outlined" color="secondary" size="small" />
+                  <Typography gutterBottom> GDGGEI35XJ7BQ6K3WLSVVFJA5JWGSIDVT4QAWAYHBG2Y3V3NLP76RC5U</Typography>
+                </Box>
+                <Box display="flex">
+                  <Chip label="ALGO" variant="outlined" color="secondary" size="small" />
+                  <Typography gutterBottom> Q3YIDNVGHNWYPPOWJE4K5UVTYGM33ADPNVRKXSTYGWAPAWADJSDZ34N6AA</Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Popover>
         </div>
       </div>
 
